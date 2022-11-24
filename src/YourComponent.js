46b6dadb79a97e29431ac4bc1bc12388bf3b8ac8
Leslie <leslie.gonzalez1799@gmail.com>
2022-11-23 22:28:46 -0600

    google maps api added

--- src/YourComponent.js
+++ src/YourComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import GoogleMaps from "simple-react-google-maps";
 /*
 * Use this component as a launching-pad to build your functionality.
 *
@@ -9,7 +9,20 @@ export default class YourComponent extends Component {
   render() {
     return (
       <div style={divStyle}>
-		  <h1> Put your solution here!</h1>
+        <h1> Put your solution here!</h1>
+        <GoogleMaps
+          apiKey={"AIzaSyCVH8e45o3d-5qmykzdhGKd1-3xYua5D2A"}
+          style={{height: "400px", width: "1000px"}}
+          zoom={10}
+          center={{
+            lat: 19.435140794267156, 
+            lng: -99.14128948071048
+          }}
+          markers={{
+            lat: 19.435140794267156, 
+            lng: -99.14128948071048
+          }}
+        />
       </div>
     );
   }
